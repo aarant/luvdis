cbf8ee90374a7365a60cad42f8d5b8a779c531f3
Ariel Antonitis <arant@mit.edu>
2020-02-27 11:36:06 -0500

    Fixed data files not being included in sdists.

--- luvdis/__init__.py
+++ luvdis/__init__.py
@@ -2,6 +2,6 @@
 
 Copyright (C) 2020 A. Antonitis. Licensed under the MIT license.
 """
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __doc__ = __doc__.replace('__version__', __version__)
 url = __url__ = 'https://github.com/arantonitis/luvdis'
